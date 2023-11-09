f05f9027fabff4f7d7ee6a521a3a42ec8db81a4b
hui1110 <hui1110rui@163.com>
2023-11-09 10:18:05 +0800

    update bash file

--- scripts/build-container-registry-image.sh
+++ scripts/build-container-registry-image.sh
@@ -1,6 +1,11 @@
-echo "build-image.sh started"
-az acr login --name $ACR_NAME
+#!/bin/bash
+
+echo "build image started"
+
+acrLoginToken=$(az acr login --name $ACR_NAME --expose-token)
+
+docker login $ACR_NAME -u 00000000-0000-0000-0000-000000000000 -p $acrLoginToken
 
 az acr build --registry $ACR_NAME --image $ACR_NAME.azurecr.io/helloacrtasks:v1 .
 
-echo "build-image.sh completed successfully!"
+echo "build image completed !"
