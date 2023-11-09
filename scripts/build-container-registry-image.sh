dd9ddf73d7e65e79ba9e7c7f0ad2932dacad3894
hui1110 <hui1110rui@163.com>
2023-11-09 10:57:42 +0800

    update docker login

--- scripts/build-container-registry-image.sh
+++ scripts/build-container-registry-image.sh
@@ -8,7 +8,7 @@ acrLoginToken=$(az acr login --name $ACR_NAME --expose-token --output tsv --quer
 
 echo "acrLoginToken: $acrLoginToken"
 
-docker login $ACR_NAME.azurecr.io --username 00000000-0000-0000-0000-000000000000 --password-stdin <<< $acrLoginToken
+docker login loginServer -u 00000000-0000-0000-0000-000000000000 -p $acrLoginToken
 
 az acr build --registry $ACR_NAME --image $ACR_NAME.azurecr.io/helloacrtasks:v1 .
 
