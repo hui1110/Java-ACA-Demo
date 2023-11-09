4cfaa4fc59f714a66d1c53977b051cbf7a21540a
hui1110 <hui1110rui@163.com>
2023-11-09 17:21:03 +0800

    update

--- scripts/build-container-registry-image.sh
+++ scripts/build-container-registry-image.sh
@@ -8,6 +8,6 @@ az acr login --name $ACR_NAME  --expose-token
 
 pwd=$(pwd)
 
-az acr build -r $ACR_NAME --image $ACR_NAME:v1 $pwd
+az acr build -r $ACR_NAME --image $ACR_NAME:v1 $pwd/Java-ACA-Demo/
 
 echo "build image completed !"
