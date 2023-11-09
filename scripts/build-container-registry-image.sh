55eccc4bd4b6a85438d14b6b76f19e8fd22e5bb3
hui1110 <hui1110rui@163.com>
2023-11-09 14:09:17 +0800

    add Dockerfile

--- scripts/build-container-registry-image.sh
+++ scripts/build-container-registry-image.sh
@@ -4,7 +4,9 @@ set -Eeuo pipefail
 
 echo "build image started"
 
-TOKEN=$(az acr login --name $ACR_NAME  --expose-token --output tsv --query accessToken)
+accessToken=$(az acr login --name $ACR_NAME  --expose-token --output tsv --query accessToken)
+
+docker login $ACR_NAME.azurecr.io -u 00000000-0000-0000-0000-000000000000 -p $accessToken
 
 az acr build --resource-group $RESOURCE_GROUP --registry $ACR_NAME --image $ACR_NAME.azurecr.io/helloacrtasks:v1 .
 
