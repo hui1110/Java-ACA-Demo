565596c13ffe9c7c0b36b763078dd42e4d64ae78
hui1110 <hui1110rui@163.com>
2023-11-09 16:51:04 +0800

    back

--- scripts/build-container-registry-image.sh
+++ scripts/build-container-registry-image.sh
@@ -6,6 +6,6 @@ echo "build image started"
 
 az acr login --name $ACR_NAME  --expose-token
 
-az acr build -r $ACR_NAME --file Dockerfile --image $ACR_NAME:v1
+az acr build -r $ACR_NAME . --file Dockerfile --image $ACR_NAME:v1
 
 echo "build image completed !"
