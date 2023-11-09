99c8abacae3e75ef6c24b7aa5c3b048118ecc56b
hui1110 <hui1110rui@163.com>
2023-11-09 12:17:03 +0800

    try to fic error

--- scripts/build-container-registry-image.sh
+++ scripts/build-container-registry-image.sh
@@ -8,8 +8,8 @@ acrLoginToken=$(az acr login --name $ACR_NAME --expose-token --output tsv --quer
 
 echo "acrLoginToken: $acrLoginToken"
 
-docker login $ACR_NAME.azurecr.io -u 00000000-0000-0000-0000-000000000000 -p $acrLoginToken
+docker login $ACR_NAME.azurecr.io --username 00000000-0000-0000-0000-000000000000 --password-stdin $acrLoginToken
 
-az acr build --registry $ACR_NAME --image $ACR_NAME.azurecr.io/helloacrtasks:v1 .
+az acr build --resource-group $RESOURCE_GROUP --registry $ACR_NAME --image $ACR_NAME.azurecr.io/helloacrtasks:v1 .
 
 echo "build image completed !"
