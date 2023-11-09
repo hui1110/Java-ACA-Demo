a51bd60cb57725accf38f37b0858cb8579c210fc
hui1110 <hui1110rui@163.com>
2023-11-09 14:13:10 +0800

    update

--- scripts/build-container-registry-image.sh
+++ scripts/build-container-registry-image.sh
@@ -6,8 +6,6 @@ echo "build image started"
 
 accessToken=$(az acr login --name $ACR_NAME  --expose-token --output tsv --query accessToken)
 
-docker login $ACR_NAME.azurecr.io -u 00000000-0000-0000-0000-000000000000 -p $accessToken
-
 az acr build --resource-group $RESOURCE_GROUP --registry $ACR_NAME --image $ACR_NAME.azurecr.io/helloacrtasks:v1 .
 
 echo "build image completed !"
