f5737bfb962a8c22a5d9f90d5f6ba61c130fba16
hui1110 <hui1110rui@163.com>
2023-11-09 16:18:03 +0800

    update

--- scripts/build-container-registry-image.sh
+++ scripts/build-container-registry-image.sh
@@ -4,8 +4,8 @@ set -Eeuo pipefail
 
 echo "build image started"
 
-accessToken=$(az acr login --name $ACR_NAME  --expose-token --output tsv --query accessToken)
+az acr login --name $ACR_NAME  --expose-token --output tsv --query accessToken
 
-az acr build --resource-group $RESOURCE_GROUP --registry $ACR_NAME --image $ACR_NAME.azurecr.io/helloacrtasks:v1 . --file ./Dockerfile
+az acr build --resource-group $RESOURCE_GROUP --registry $ACR_NAME https://github.com/hui1110/Java-ACA-Demo.git --image $ACR_NAME:v1 -f Windows.Dockerfile --platform windows
 
 echo "build image completed !"
