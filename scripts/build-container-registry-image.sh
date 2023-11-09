8f6a05ecc3bffd3f3c017f9d9fbe65e13cd8cf8d
hui1110 <hui1110rui@163.com>
2023-11-09 10:50:15 +0800

    update template file and bash file

--- scripts/build-container-registry-image.sh
+++ scripts/build-container-registry-image.sh
@@ -4,11 +4,11 @@ set -Eeuo pipefail
 
 echo "build image started"
 
-acrLoginToken=$(az acr login --name $ACR_NAME --expose-token)
+acrLoginToken=$(az acr login --name $ACR_NAME --expose-token --output tsv --query accessToken)
 
 echo "acrLoginToken: $acrLoginToken"
 
-docker login $ACR_NAME -u 00000000-0000-0000-0000-000000000000 -p $acrLoginToken
+docker login $ACR_NAME.azurecr.io --username 00000000-0000-0000-0000-000000000000 --password-stdin <<< $acrLoginToken
 
 az acr build --registry $ACR_NAME --image $ACR_NAME.azurecr.io/helloacrtasks:v1 .
 
