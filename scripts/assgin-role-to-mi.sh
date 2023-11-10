5776dddfece9c562e152ebcc3bf5662486588a44
hui1110 <hui1110rui@163.com>
2023-11-10 15:01:42 +0800

    test

--- scripts/assgin-role-to-mi.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-set -Eeuo pipefail
-
-az role assignment create --assignee "$SPID" --role acrpull --scope /subscriptions/"$SUBSCRIPTION_ID"/resourceGroups/"$RESOURCE_GROUP"/providers/Microsoft.ContainerRegistry/registries/"$ACR_NAME"
