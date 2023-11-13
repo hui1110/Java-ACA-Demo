f2efb25506760690439994544bbdfb7b82247784
hui1110 <hui1110rui@163.com>
2023-11-13 11:29:32 +0800

    update

--- scripts/build-container-registry-image.sh
+++ scripts/build-container-registry-image.sh
@@ -4,4 +4,4 @@ set -Eeuo pipefail
 
 az acr login --name $ACR_NAME  --expose-token
 
-az acr build --registry $ACR_NAME --image $ACR_NAME:v1 ../ --file Dockerfile
+az acr build --registry $ACR_NAME --image $ACR_NAME:v1 ../ --file ../Dockerfile
