615882ea854893a6856f49a7a2a82fab900d1e95
hui1110 <hui1110rui@163.com>
2023-11-14 13:08:15 +0800

    update

--- scripts/build-container-registry-image.sh
+++ scripts/build-container-registry-image.sh
@@ -2,10 +2,10 @@
 
 set -Eeuo pipefail
 
-rm -rf Java-ACA-Demo
-
-git clone https://github.com/hui1110/Java-ACA-Demo.git
+#rm -rf Java-ACA-Demo
+#
+#git clone https://github.com/hui1110/Java-ACA-Demo.git
 
 az acr login --name $ACR_NAME  --expose-token
 
-az acr build --registry $ACR_NAME --image $ACR_NAME:v1 ./Java-ACA-Demo
+az acr build --registry $ACR_NAME --image $ACR_NAME:v1 https://github.com/hui1110/Java-ACA-Demo.git
