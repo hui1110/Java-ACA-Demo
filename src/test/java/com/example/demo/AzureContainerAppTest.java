89e87d684181cde41f2e67e92d9e4db8d7234aa0
hui1110 <hui1110rui@163.com>
2023-11-16 13:50:44 +0800

    update

--- src/test/java/com/example/demo/AzureContainerAppTest.java
+++ src/test/java/com/example/demo/AzureContainerAppTest.java
@@ -9,14 +9,14 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Objects;
 
 public class AzureContainerAppTest {
-    private static final String FQDN = "";
+    private static final String FQDN = "app-armacatest--revision.bluemoss-dc4559ed.eastus.azurecontainerapps.io";
 
     @Test
     void testCreatedContainerApp() {
         RestTemplate restTemplate = new RestTemplate();
 
-        String controllerUrl = String.format("https://%s/hello", FQDN);
-        ResponseEntity<String> controllerResponse = restTemplate.getForEntity(controllerUrl, String.class);
+        String url = String.format("https://%s/hello", FQDN);
+        ResponseEntity<String> controllerResponse = restTemplate.getForEntity(url, String.class);
         Assert.isTrue(Objects.equals(controllerResponse.getBody(), "Hello World! Java 8"), "Response body is not expected");
 
         String attachUrl = String.format("https://%s/attach", FQDN);
