adb9b79bf325ce308d2bec563b7bbda8c099204c
hui1110 <hui1110rui@163.com>
2023-12-05 16:34:06 +0800

    add  sample code

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,80 +1,46 @@
 package com.example.demo.controller;
 
-import com.sun.tools.attach.VirtualMachine;
-import com.sun.tools.attach.VirtualMachineDescriptor;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HelloController {
 
-    String AGENT_ARGS = "app.jar";
+    private static final org.slf4j.Logger LOGGER = LoggerFactory.getLogger(HelloController.class);
 
-    String AGENT_PATH = "https://huiagentaccount.blob.core.windows.net/agent-8/java-accelerator-agent-0.0.1-SNAPSHOT.jar";
-
-    @GetMapping("/premain")
-    public String premain() {
-        return "premain method";
+    @GetMapping("/hello")
+    public String hello() {
+        LOGGER.debug("Debug hello world");
+        return "hello world";
     }
 
-    @GetMapping("/agentmain")
-    public String agentmain() {
-        return "agentmain method";
+    @GetMapping("/logger")
+    public Object getLogger() {
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        return loggerContext.getLoggerList().stream().filter(logger ->
+                logger.getName().equals(HelloController.class.getName())).map(logger -> {
+            Map<String, String> loggerMap = new java.util.HashMap<>();
+            loggerMap.put("name", logger.getName());
+            loggerMap.put("level", logger.getLevel().toString());
+            return loggerMap;
+        }).collect(java.util.stream.Collectors.toList());
     }
 
-    @RequestMapping(value = "/attach", method = RequestMethod.GET)
-    public String updateClassMethod() {
-
-        System.out.println("Application SystemClassLoader:" + ClassLoader.getSystemClassLoader());
-        System.out.println("Application ClassLoader:" + HelloController.class.getClassLoader());
-        try {
-            List<VirtualMachineDescriptor> list = VirtualMachine.list();
-            for (VirtualMachineDescriptor vmd : list) {
-                System.out.println(vmd.id() + " : " + vmd.displayName());
-                if (vmd.displayName().equals(AGENT_ARGS)) {
-                    System.out.println("find agent process");
-                    String agentFilePath = downloadAgentJar().getAbsolutePath();
-                    VirtualMachine virtualMachine = VirtualMachine.attach(vmd.id());
-                    virtualMachine.loadAgent(agentFilePath);
-                    virtualMachine.detach();
-                }
-            }
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        return "attach success";
-    }
-
-    private File downloadAgentJar() throws IOException, URISyntaxException {
-        String[] split = AGENT_PATH.split("/");
-        String fileName = split[split.length - 1];
-        File file = new File(fileName);
-        if (file.exists()) {
-            file.delete();
-        }
-        HttpURLConnection connection = (HttpURLConnection) new URI(AGENT_PATH).toURL().openConnection();
-        connection.connect();
-        try (InputStream inputStream = connection.getInputStream();
-             OutputStream outputStream = Files.newOutputStream(file.toPath())) {
-             IOUtils.copy(inputStream, outputStream);
-        } finally {
-            connection.disconnect();
-        }
-        System.out.println("---download agent jar success---");
-        return file;
+    @GetMapping(path = "/updateLogLevel")
+    public String updateLogLevel(@RequestParam("level") String level) {
+        final String loggerName = HelloController.class.getName();
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        Logger logger = loggerContext.getLogger(loggerName);
+        Level newLevel = Level.toLevel(level, null);
+        logger.setLevel(newLevel);
+        return "OK";
     }
 
 }
